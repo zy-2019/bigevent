fb668a0648f0e10fcc41283b562a3992918658f1
Your Name <you@example.com>
2019-11-20 16:58:21 +0800

    user处理数据代码分离

--- web_back/js/means/user.js
+++ web_back/js/means/user.js
@@ -1,51 +1,35 @@
 
-// 提取公共的接口获取url地址
+// 提取公共的接口获取url地址 以全局变量的格式
 
 var baseUrl = 'http://localhost:8000/'
 
 var user = {
 
-    login : function (myname,mypassword) {
+    login : function (myname,mypassword,callback) {
 
             $.post(baseUrl +'admin/login', {user_name : myname , password : mypassword} , function (res) {
             
-            console.log(res);
+                callback(res)
+            // console.log(res);
             
-            if (res.code === 200) {
-                alert('登录成功')
-                window.location.href = './index.html'
-            }
-            else if (res.code === 400) {
-                // alert(res.msg)
-                $('#msgInfo').text(res.msg)
-                $('#myModal').modal('show')
-            }
         })
     },
 
-    logout : function () {
+    logout : function (callback) {
         
         $.post(baseUrl +'admin/logout', function (res) {
-            console.log(res);
-
-            if (res.code === 200) {
-                window.location.href = './login.html'
-            }   
-
+            
+            callback(res)
         })
     },
 
-    getInfo: function () {
+    getInfo: function (callback) {
         $.get(baseUrl +'admin/getuser', function (res) {
 
-            console.log(res);
+            callback(res)
+            // console.log(res);
 
-            if(res.code === 200){
-
-                $(".userimg").prop('src', res.data.user_pic)
-
-                $('#username').text(res.data.nickname)
-            }
+            
 
         })
 
