e5c23a316d8d4505e64a0a897c4d7b5a3bb19936
Your Name <you@example.com>
2019-11-21 23:35:46 +0800

    文章类别管理初次提交

---
+++ web_back/js/means/config.js
@@ -0,0 +1,22 @@
+
+// 公共接口地址放在一起 方便管理 方便改动
+
+var baseUrl = 'http://localhost:8000/'
+
+var APIURLS = {
+
+    jklogin : baseUrl +'admin/login' ,
+
+    jklogout : baseUrl +'admin/logout' ,
+
+    jkgetinfo : baseUrl +'admin/getuser',
+
+
+
+    category_show:  baseUrl + 'admin/category_search',
+    category_del : baseUrl + 'admin/category_delete',
+    category_add : baseUrl + 'admin/category_add',
+    category_edit : baseUrl + 'admin/category_edit'
+
+
+}
--- web_back/js/means/user.js
+++ web_back/js/means/user.js
@@ -1,35 +1,37 @@
 
 // 提取公共的接口获取url地址 以全局变量的格式
 
-var baseUrl = 'http://localhost:8000/'
+// var baseUrl = 'http://localhost:8000/' 
+
+// 放到一个新js文件统一引用管理
 
 var user = {
 
+ // 发请求拿数据
+//  把数据返回去 谁用谁去处理数据
+    
     login : function (myname,mypassword,callback) {
 
-            $.post(baseUrl +'admin/login', {user_name : myname , password : mypassword} , function (res) {
+            $.post(APIURLS.jklogin , {user_name : myname , password : mypassword} , function (res) {
             
                 callback(res)
             // console.log(res);
-            
         })
     },
 
     logout : function (callback) {
         
-        $.post(baseUrl +'admin/logout', function (res) {
+        $.post(APIURLS.jklogout , function (res) {
             
             callback(res)
         })
     },
 
     getInfo: function (callback) {
-        $.get(baseUrl +'admin/getuser', function (res) {
+        $.get(APIURLS.jkgetinfo, function (res) {
 
             callback(res)
             // console.log(res);
-
-            
 
         })
 
